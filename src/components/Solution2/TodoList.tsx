82b90a7befb49f478aa6404aa34e9753012d455d
Kevin <kevinsz2805@gmail.com>
2020-07-14 08:50:13 -0600

    adding hello world and .gitignore

--- src/components/Solution2/TodoList.tsx
+++ src/components/Solution2/TodoList.tsx
@@ -7,23 +7,24 @@ const TodoList = (props: TodoProps) => {
       <div className="col-lg-12">
         <br />
       </div>
-      <h2>TODO List</h2>
-
-      <div className="form-check">
-        {props.data.map((item: ItemsProps, index: number) => {
-          return (
-            <div key={item.id}>
-              <input
-                type="checkbox"
-                className="form-check-input"
-                value={index}
-                checked={item.done}
-                onChange={(e: any) => props.onChange(e, item)}
-              />
-              <label>{item.text}</label>
-            </div>
-          );
-        })}
+      <div className="col-lg-12">
+        <h2>TODO List</h2>
+        <div className="form-check">
+          {props.data.map((item: ItemsProps, index: number) => {
+            return (
+              <div key={item.id}>
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  value={index}
+                  checked={item.done}
+                  onChange={(e: any) => props.onChange(e, item)}
+                />
+                <label>{item.text}</label>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
